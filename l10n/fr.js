28100e354d2777df10166f14f103d0ca3a3ac646
Patrick Paysant <patrick.paysant@linagora.com>
2016-10-26 08:09:03 +0200

    Add l10N for OC9

---
+++ l10n/fr.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "gatekeeper",
+    {
+        "Select Mode" : "Selectionnez un mode",
+        "White List" : "Liste blanche",
+        "Only groups in this white list are allowed EXCEPT those in EXCLUSION list" : "Tous les groupes de la liste blanche sont autorisés SAUF ceux de la liste d'exclusion",
+        "Black List" : "Liste noire",
+        "Only groups in this black list are denied PLUS those in EXCLUSION list" : "Tous les groupes de la liste noire sont refusés EN plus de ceux de la liste d'exclusion",
+        "Minimal" : "Minimaliste",
+        "Only groups in EXCLUSION list are denied" : "Tous les groupes dans la liste d'exclusion sont refusés",
+        "Opengate" : "Porte ouverte",
+        "No checking is done" : "Aucune vérification n'est faite",
+        "Exclusion List" : "Liste d'exclusion",
+        "Enter Group Name" : "Entrez le nom du groupe",
+        "Add to list" : "Ajoutez à la liste",
+        "Display List" : "Affichez la liste",
+        "Mode {0} is selected" : "{0} est sélectionné",
+        "Group {0} is removed from {1}" : "Le groupe {0} est supprimé de {1}",
+        "Group {0} is added in {1}" : "Le groupe {0} est ajouté à {1}",
+        "You do not have access to this service." : "Vous n'avez pas accès à ce service.",
+        "Please contact your administrator with theses informations: " : "Merci de contacter votre administrateur avec ces informations: ",
+        "denied with uid=%s IP=%s." : "Interdit avec l'identifiant %s, adresse IP %s",
+        "excluded with uid=%s IP=%s." : "Rejeté avec l'identifiant %s, adresse IP %s",
+        "not allowed with uid=%s IP=%s."  : "Non autorisé avec l'identifiant %s, adresse IP %s"
+    },
+    "nplurals=2; plural=(n > 1);"
+);
